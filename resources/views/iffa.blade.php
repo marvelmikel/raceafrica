ca8f2abcd7b21f4c4a8a950bde5e5af95a112b25
Ejeh Micheal Marvelous <49599133+marvelmikel@users.noreply.github.com>
2024-03-19 12:21:49 +0000

    Update iffa.blade.php

--- resources/views/iffa.blade.php
+++ resources/views/iffa.blade.php
@@ -7,7 +7,7 @@
     <title>IFFA-SUMMIT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="{{asset('a/css/main.css')}}"/>
-      <link rel="stylesheet" href="{{asset('a/scss/main.css')}}" />
+<!--       <link rel="stylesheet" href="{{asset('a/scss/main.css')}}" /> -->
 
     <!-- font awesome cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
