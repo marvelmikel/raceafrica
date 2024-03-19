bca469167720ce4166b53d9367b501885fc31098
Ejeh Micheal Marvelous <49599133+marvelmikel@users.noreply.github.com>
2024-03-19 12:25:58 +0000

    Update iffa.blade.php

--- resources/views/iffa.blade.php
+++ resources/views/iffa.blade.php
@@ -5,12 +5,18 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>IFFA-SUMMIT</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="{{asset('a/css/main.css')}}"/>
-<!--       <link rel="stylesheet" href="{{asset('a/scss/main.css')}}" /> -->
+   <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="{{asset('a/scss/main.css')}}" />
 
     <!-- font awesome cdn link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    />
+    <link rel="shortcut icon" href="{{asset('a/images/logo2.png')}}" type="image/x-icon" />
 </head>
 
 <body>
