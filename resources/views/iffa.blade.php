3207b1dabafef3edaa52b8117b3c0001b4107f77
Ejeh Micheal Marvelous <49599133+marvelmikel@users.noreply.github.com>
2024-04-01 09:58:18 +0100

    button fixes

--- resources/views/iffa.blade.php
+++ resources/views/iffa.blade.php
@@ -887,36 +887,10 @@
 
     joinUsBtn.addEventListener('click', function (e) {
         e.preventDefault();
-        var sectionOffset = regSection.offsetTop;
-        scrollToSmoothly(sectionOffset, 1000);
+        regSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
     });
-
-    function scrollToSmoothly(to, duration) {
-        var element = document.scrollingElement || document.documentElement,
-            start = element.scrollTop,
-            change = to - start,
-            currentTime = 0,
-            increment = 20;
-
-        var animateScroll = function () {
-            currentTime += increment;
-            var val = Math.easeInOutQuad(currentTime, start, change, duration);
-            element.scrollTop = val;
-            if (currentTime < duration) {
-                setTimeout(animateScroll, increment);
-            }
-        };
-        animateScroll();
-    }
-
-    // easing functions for smooth scrolling
-    Math.easeInOutQuad = function (t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
-    };
 });
+
 
     </script>
 
